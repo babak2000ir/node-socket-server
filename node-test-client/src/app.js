28b83ffca95a53a9c11e9a2262c9107ff8844db3
Babak Jahedmanesh <babak2000ir@gmail.com>
2025-02-09 16:43:19 +0000

    node client folder rename

---
+++ node-test-client/src/app.js
@@ -0,0 +1,22 @@
+import net from 'net';
+
+const port = 60992;
+const host = '192.168.1.20';
+
+const client = new net.Socket();
+client.connect(port, host, function () {
+  console.log('Connected');
+  client.write('Hello, server! Love, Client.');
+});
+
+client.on('data', function (data) {
+  console.log('Received: ' + data);
+  if (data.toString().includes('~¬DROP¬~')) {
+    client.destroy(); // kill client after server says DROP
+  }
+});
+
+client.on('close', function () {
+  console.log('Connection closed');
+});
+
